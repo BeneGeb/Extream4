09806435287c0ff4e98e878ac9a2a5c9be8d67e6
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2022-11-02 15:16:00 +0100

    structure project to packages

---
+++ Game.py
@@ -0,0 +1,71 @@
+import pygame
+
+pygame.init()
+
+ORANGE  = ( 255, 140, 0)
+ROT     = ( 255, 0, 0)
+GRUEN   = ( 0, 255, 0)
+SCHWARZ = ( 0, 0, 0)
+WEISS   = ( 255, 255, 255)
+
+class Game:
+    def runGame(self):
+        screen = pygame.display.set_mode((640, 480))
+
+        pygame.display.set_caption("Unser erstes Pygame-Spiel")
+
+        gameActive = True
+
+        # Set up timer
+        clock = pygame.time.Clock()
+
+
+        while gameActive:
+            # UserInteraction
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameActive = False
+                    print("Spieler hat Quit-Button angeklickt")
+                elif event.type == pygame.KEYDOWN:
+                    print("Spieler hat Taste gedrückt")
+
+                
+                    if event.key == pygame.K_RIGHT:
+                        print("Spieler hat Pfeiltaste rechts gedrückt")
+                    elif event.key == pygame.K_LEFT:
+                        print("Spieler hat Pfeiltaste links gedrückt")
+                    elif event.key == pygame.K_UP:
+                        print("Spieler hat Pfeiltaste hoch gedrückt")
+                    elif event.key == pygame.K_DOWN:
+                        print("Spieler hat Pfeiltaste runter gedrückt")
+                    elif event.key == pygame.K_SPACE:
+                        print("Spieler hat Leertaste gedrückt")
+
+                    elif event.key == pygame.K_w:
+                        print("Spieler hat Taste w gedrückt")
+                    elif event.key == pygame.K_a:
+                        print("Spieler hat Taste a gedrückt")
+                    elif event.key == pygame.K_s:
+                        print("Spieler hat Taste s gedrückt")
+                    elif event.key == pygame.K_d:
+                        print("Spieler hat Taste d gedrückt")
+
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print("Spieler hast Maus angeklickt")
+
+            # Gamelogic
+
+            # Fill Display
+            screen.fill(WEISS)
+
+            # Draw Structures and Figures
+
+            # Update Disply
+            pygame.display.flip()
+
+            # Setupr refreshtimer
+            clock.tick(60)
+
+        pygame.quit()
+
+
