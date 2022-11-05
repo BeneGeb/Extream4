7f752ed9ea88a1cfa06204a4c30db9261aa1d50d
Samuel2k1 <91870323+Samuel2k1@users.noreply.github.com>
2022-11-05 14:28:50 +0100

    Würfel in implementiert
    Eine Animation fehlt noch

--- game/Game.py
+++ game/Game.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
 
@@ -9,13 +10,44 @@ GRUEN   = ( 0, 255, 0)
 SCHWARZ = ( 0, 0, 0)
 WEISS   = ( 255, 255, 255)
 
+# Würfel Klasse
+
+# WürfelAugenbilder mit Skalierung
+dice1 = pygame.image.load("Würfel_1.png")
+dice1 = pygame.transform.scale(dice1, (100,100))
+dice2 = pygame.image.load("Würfel_2.png")
+dice2 = pygame.transform.scale(dice2, (100,100))
+dice3 = pygame.image.load("Würfel_3.png")
+dice3 = pygame.transform.scale(dice3, (100,100))
+dice4 = pygame.image.load("Würfel_4.png")
+dice4 = pygame.transform.scale(dice4, (100,100))
+dice5 = pygame.image.load("Würfel_5.png")
+dice5 = pygame.transform.scale(dice5, (100,100))
+dice6 = pygame.image.load("Würfel_6.png")
+dice6 = pygame.transform.scale(dice6, (100,100))
+
+# Liste der Würfelaugen
+all_dice = [ None, dice1, dice2, dice3, dice4, dice5, dice6 ]
+pygame.display.set_icon( dice6 )
+
+def Wuerfelwurf():
+    wurf   = random.randint( 1, 6 )
+
+    return wurf
+
+
+
 class Game:
     def runGame(self):
-        screen = pygame.display.set_mode((1142, 1008))
+        wurf = 0
+        wuerfelaugen = None
+        laenge = 1142
+        breite = 1008
+        screen = pygame.display.set_mode((laenge, breite))
         
         #Hintergrund
         bg = pygame.image.load("Spielbrett.png")
-        bg = pygame.transform.scale(bg,(1142,1008))
+        bg = pygame.transform.scale(bg,(laenge, breite))
 
         pygame.display.set_caption("Unser erstes Pygame-Spiel")
 
@@ -58,12 +90,17 @@ class Game:
                         print("Spieler hat Taste d gedrückt")
 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    print("Spieler hast Maus angeklickt")
+                    wurf = Wuerfelwurf()
+                    wuerfelaugen = all_dice[wurf]
+                    print(f"Spieler hat {wurf} gerollt")
+                
+            # Würfelbild und Koordinaten des Würfels
+            if ( wuerfelaugen != None ):
+                screen.blit( wuerfelaugen, ( 0, 0) ) 
 
             # Gamelogic
 
-            # Fill Display
-            #screen.fill(WEISS)
+            
 
             # Draw Structures and Figures
 
