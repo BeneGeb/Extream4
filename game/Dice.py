8c848d5cfb40a5e85e77ddd30b72824af3491be7
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2022-11-07 10:42:47 +0100

    Update Dice.py

--- game/Dice.py
+++ game/Dice.py
@@ -4,38 +4,63 @@ import random
 pygame.init()
 
 
-class Dice():
-    def __init__(self,screen, position):
+class Dice:
+    def __init__(self, screen, position, size):
         self.screen = screen
         self.position = position
-        self.currentValue = 1
+        self.currentValue = 6
+        self.size = size
         self.loadImages()
-        
 
     def loadImages(self):
         all_dice = []
-        for i in range(1,7):
+        all_dice.append(None)
+        for i in range(1, 7):
             nextImage = pygame.image.load("Würfel_" + str(i) + ".png")
-            print("Würfel_" + str(i) + ".png")
-            nextImage = pygame.transform.scale(nextImage, (100,100))
+            nextImage = pygame.transform.scale(nextImage, (self.size, self.size))
             all_dice.append(nextImage)
         self.all_dice = all_dice
 
     def rollDice(self):
-        dice  = random.randint( 1, 6 )
-        return dice
+        self.currentValue = random.randint(1, 6)
 
     def draw(self):
-        print(self.currentValue)
-        self.screen.blit(self.all_dice[self.currentValue], self.position)
+        self.screen.blit(self.all_dice[self.currentValue], (0, 0))
 
-        
+    def handleClick(self, clickedPos):
+        diceX, diceY = self.position
+        clickedX, clickedY = clickedPos
+
+        if (
+            diceX <= clickedX <= diceX + self.size
+            and diceY <= clickedY <= diceY + self.size
+        ):
+            self.rollDice()
+
+    def getDiceValue(self):
+        return self.currentValue
 
 
-    
 screen = pygame.display.set_mode((1000, 1000))
-dice = Dice(screen, (0,0))
+dice = Dice(screen, (0, 0), 200)
+gameActive = True
 
-while True:
+dice1 = pygame.image.load("Würfel_1.png")
+dice1 = pygame.transform.scale(dice1, (100, 100))
+
+clock = pygame.time.Clock()
+
+while gameActive:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameActive = False
+            print("Spieler hat Quit-Button angeklickt")
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                dice.rollDice()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            dice.handleClick(pygame.mouse.get_pos())
+
     dice.draw()
-
+    pygame.display.flip()
+    clock.tick(60)
