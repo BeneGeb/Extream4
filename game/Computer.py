db299e7eda679cb6d63766ab485dff45b53a51c8
BeneGeb <92448111+BeneGeb@users.noreply.github.com>
2023-04-28 13:43:08 +0200

    s

--- game/Computer.py
+++ game/Computer.py
@@ -2,5 +2,8 @@ class Computer:
     def __init__(self, playerNumber):
         self.playerNumber = playerNumber
 
-    def getAllFigures(allFigures, allCircles):
-        return []
+    def getAllFigures(self, allFigures):
+        return [figure for figure in allFigures if figure.player == self.playerNumber]
+
+    def evalNextMove(self, gameField):
+        self.allTeamFigure = self.getAllFigures(gameField.allFigures)
